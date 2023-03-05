7d40e1c963dbc6d25a55f23b0c1592d1ec724ed1
KUZNETSOV ALEKSANDR <125797734+Muragon84@users.noreply.github.com>
2023-03-05 23:11:04 +0300

    Update 03_hw.py

--- Module4/home_work/02_list/03_hw.py
+++ Module4/home_work/02_list/03_hw.py
@@ -2,3 +2,22 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+
+import random
+
+n = int(input("Ведите количество элементов n: "))
+numbers = []
+if n >= 0:
+    for i in range(n):
+        numbers.append(random.randint(-100, 100))
+        i += 1
+    print(numbers)
+else:
+    print("n должно быть больше нуля")
+
+summa = 0
+for element in numbers:
+     if element >= 0 and element % 2 == 0:
+        summa += element
+
+print("Cумма всех положительных элементов кратных двум: ", summa)
