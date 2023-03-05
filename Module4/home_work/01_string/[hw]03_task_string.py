1197098d66ea318d2d00dc8431aa2b9cb39a7570
KUZNETSOV ALEKSANDR <125797734+Muragon84@users.noreply.github.com>
2023-03-05 21:11:36 +0300

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,14 @@
 # Посчитайте количество согласных букв в данной строке.
 
 text = ...
+
+text = "В теории, теория и практика неразделимы. На практике это не так."
+text_new = text.lower()
+vowels = set("бвгджзйклмнпрстфхцчшще")
+count = 0
+
+for letter in text_new:
+    if letter in vowels:
+        count += 1
+
+print("Согласных букв: ", count)
