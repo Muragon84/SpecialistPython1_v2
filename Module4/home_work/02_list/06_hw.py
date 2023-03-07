d3f728add54f74937d0f373073deb770835b00b5
KUZNETSOV ALEKSANDR <125797734+Muragon84@users.noreply.github.com>
2023-03-07 14:40:23 +0300

    Update 06_hw.py

--- Module4/home_work/02_list/06_hw.py
+++ Module4/home_work/02_list/06_hw.py
@@ -34,12 +34,45 @@ items = [
 # Найдите:
 print("Товары на складе представлены брэндами: ")
 
-# TODO: your code here
+all_brends = []
+for brands in items:
+    all_brends.append(brands["brand"])
+list_brends = []
+unique_brends = set(all_brends)
+for number in unique_brends:
+    list_brends.append(number)
+print(list_brends)
 
 print("На складе больше всего товаров брэнда(ов): ")
 
-# TODO: your code here
+count_adidas = all_brends.count('adidas')
+count_reebok = all_brends.count('reebok')
+count_puma = all_brends.count('puma')
+if count_adidas > count_reebok and count_adidas > count_puma:
+     print(f'Больше товаров бренда Adidas, {count_adidas} шт.')
+elif count_puma > count_reebok and count_adidas < count_puma:
+     print(f'Больше товаров бренда Puma, {count_puma} шт.')
+elif count_puma < count_reebok and count_adidas < count_reebok:
+    print(f'Больше товаров бренда Reebok, {count_reebok} шт.')
+elif count_puma == count_reebok and count_adidas == count_puma:
+     print(f"Товаров всех брендов равное количество, {count_adidas} шт.")
+elif count_puma == count_reebok and count_adidas < count_reebok:
+     print(f'Больше товаров бренда Puma и Reebok, по {count_puma} шт.')
+elif count_adidas == count_reebok and count_adidas > count_puma:
+     print(f'Больше товаров бренда Adidas и Reebok, по {count_adidas} шт.')
+elif count_adidas == count_puma and count_adidas > count_reebok:
+     print(f'Больше товаров бренда Adidas и Puma, по {count_adidas} шт.')
 
 print("На складе самый дорогой товар брэнда(ов): ")
 
-# TODO: your code here
+list_price = []
+for i in items:
+    list_price.append(i['price'])
+max_price = max(list_price)
+for i in items:
+    if max_price in i.values():
+        name = i['name']
+        price = i['price']
+        brand = i['brand']
+        print(f'Самый дорогой шмот {name}, его цена {price} УбиенныхЕнотов, а штопает {brand}')
+# ВОПРОС: Почему не работает вывод вида - print(f'Самый дорогой шмот {i['name']}, его цена {i['price']} УбиенныхЕнотов, а штопает {i['brand']}')
