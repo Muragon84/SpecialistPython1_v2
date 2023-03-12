f00f08931bfafbfa3e6f3b17e2e3a485a397399c
KUZNETSOV ALEKSANDR <125797734+Muragon84@users.noreply.github.com>
2023-03-12 22:07:28 +0300

    Update 02_task_dict.py

--- Module5/practice/dict/02_task_dict.py
+++ Module5/practice/dict/02_task_dict.py
@@ -7,6 +7,12 @@ items = [
 ]
 
 # 1. Выведите нумерованный список именований всех товаров
-...
+for num, item in enumerate(items, 1):
+    print(num, item["name"])
 # 2. Выведите цену самого дешевого товара
-...
+min_price = float(items[0]["price"])
+for item in items:
+    if float(item["price"]) < min_price:
+        min_price = float(item["price"])
+
+print('Самый дешовый товар: ', min_price)
