4103950f86b9090b33bb9100a9ab7a23dcc91aa5
KUZNETSOV ALEKSANDR <125797734+Muragon84@users.noreply.github.com>
2023-03-12 22:06:17 +0300

    Update 04_task_dict.py

--- Module5/practice/dict/04_task_dict.py
+++ Module5/practice/dict/04_task_dict.py
@@ -29,20 +29,50 @@ staff = [
 # Вычислите:
 print("Имя и Фамилию сотрудника с самой высокой зарплатой:")
 
-# TODO: your code here
+max_employee = staff[0]
+for employee in staff:
+    if employee["salary"] > max_employee["salary"]:
+        max_employee = employee
+print(f'Самая высокая зарплата: {max_employee["name"]} {max_employee["surname"]}')
 
 print("Имя и Фамилию сотрудника с самой низкой зарплатой:")
 
-# TODO: your code here
+min_employee = staff[0]
+for employee in staff:
+    if employee["salary"] < min_employee["salary"]:
+        min_employee = employee
+print(f'Самая низкая зарплата: {min_employee["name"]} {min_employee["surname"]}')
 
 print("Среднеарифметическую зарплату всех сотрудников")
 
-# TODO: your code here
+all_salary = 0
+for salary in staff:
+    all_salary += salary["salary"]
+print(f'Среднеарифметическая зарплата всех сотрудников: {round(all_salary / len(staff), 2)}')
 
 print("Количество однофамильцев в организации")
 
-# TODO: your code here
+surnames = []
+counter = {}
+result = 0
+
+for surname in staff:
+    surnames.append(surname["surname"])
+for last_surname in surnames:
+    if last_surname in counter:
+        if counter[last_surname] == 1:
+            result += 2
+        else:
+            result += 1
+        counter[last_surname] += 1
+    else:
+        counter[last_surname] = 1
+
+print(f'Количество однофамильцев в организации: {result}')
+print(f'Сколько штук одной фамилии: {counter}')
 
 print("*Список всех сотрудников(Имя и Фамилию) в порядке возрастания их зарплаты")
 
-# TODO: your code here
+staff.sort(key=lambda dictionary: dictionary['salary'])
+for num, employee in enumerate(staff, 1):
+    print(num, employee["name"], employee["surname"])
