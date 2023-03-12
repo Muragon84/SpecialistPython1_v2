b7a785483ba9295f7322745e24cd616322ee3c46
KUZNETSOV ALEKSANDR <125797734+Muragon84@users.noreply.github.com>
2023-03-12 15:21:13 +0300

    Update 01_task_func.py

--- Module6/practice/funcs_part1/01_task_func.py
+++ Module6/practice/funcs_part1/01_task_func.py
@@ -1,8 +1,9 @@
 # Напишите функцию, возвращающую наибольшее из четырех чисел
 
 def max4(n1, n2, n3, n4):
-    # TODO: your code here
-    pass
+    max = [n1, n2, n3, n4]
+    max.sort()
+    return max[-1]
 
 
 # Тестируем функцию
